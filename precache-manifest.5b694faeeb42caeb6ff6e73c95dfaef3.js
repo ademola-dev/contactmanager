09dda27398067eaada4e20634114431c2fb76eb9
ademola-dev <ardhey101@gmail.com>
2020-10-14 21:08:36 +0100

    Updates

---
+++ precache-manifest.5b694faeeb42caeb6ff6e73c95dfaef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1d20ac3f8b4ab37be256da816f95a8",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ccb192e1a4fb3e101afd",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "8b64a4adbd6d502bd9cd",
+    "url": "/contactmanager/static/js/2.d1cb8a19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactmanager/static/js/2.d1cb8a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb192e1a4fb3e101afd",
+    "url": "/contactmanager/static/js/main.b6e8450d.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
